e9d9dd8befed2968c393db0f4e2e4b02ce31e479
Mike Mueller <mmueller@localhost>
2015-08-08 22:02:35 +0200

    First complete preprocessing.

--- crosswater/preprocessing/hdf_input.py
+++ crosswater/preprocessing/hdf_input.py
@@ -3,6 +3,7 @@
 import tables
 
 from crosswater.read_config import read_config
+from crosswater.tools.csv_reader import CsvReader
 from crosswater.tools import dbflib
 
 
@@ -59,6 +60,7 @@ def filter_strahler_lessthan_three(strahler, tot_areas, appl_areas):
     return (apply_filter(strahler), apply_filter(tot_areas),
             apply_filter(appl_areas))
 
+
 class Parameters(tables.IsDescription):
     """Table layout for parameters."""
     name = tables.StringCol(100)
@@ -96,19 +98,50 @@ def create_hdf_file(file_name, tot_areas, appl_areas):
     h5_file.close()
 
 
+def add_input_tables(h5_file_name, t_file_name, p_file_name, q_file_name):
+    h5_file = tables.open_file(h5_file_name, mode='a')
+    reader = CsvReader(t_file_name, p_file_name, q_file_name)
+    table_name = 'input'
+    counter = 0
+    for catchments in reader:
+        counter += 1
+        print(counter)
+        for group in h5_file.walk_nodes('/', 'Group'):
+            name = group._v_name
+            if name.startswith('catch_'):
+                id_ = int(name.split('_')[1])
+                row_data = catchments[id_]
+                try:
+                    table = group._f_get_child(table_name)
+                except tables.NoSuchNodeError:
+                    table = h5_file.create_table(group, table_name, Input,
+                                               'time varying inputs')
+                row = table.row
+                row_names = ['datetime', 'temperature', 'precipitation',
+                             'discharge']
+                for row_name, row_value in zip(row_names, row_data):
+                    row[row_name] = row_value
+                row.append()
+    h5_file.close()
+
+
+
+
 def preprocess(config_file):
     config = read_config(config_file)
+    h5_file_name = config['preprocessing']['hdf_input_path']
+    t_file_name = config['preprocessing']['discharge_path']
+    p_file_name = config['preprocessing']['temperature_path']
+    q_file_name = config['preprocessing']['precipitation_path']
+
     strahler = get_strahler(config['preprocessing']['catchment_path'])
     tot_areas = get_tot_areas(config['preprocessing']['catchment_path'])
     appl_areas = get_appl_areas(config['preprocessing']['landuse_path'])
     strahler, tot_areas, appl_areas = filter_strahler_lessthan_three(
         strahler, tot_areas, appl_areas)
-    print(len(strahler))
-    create_hdf_file(config['preprocessing']['hdf_input_path'],
-                    tot_areas, appl_areas)
-    #print(len(tot_areas))
-    #print(len(appl_areas))
-    #
+    create_hdf_file(h5_file_name, tot_areas, appl_areas)
+    add_input_tables(h5_file_name, t_file_name, p_file_name, q_file_name)
+
 
 
 
@@ -116,5 +149,9 @@ def preprocess(config_file):
 if __name__ == '__main__':
 
     import sys
+    import timeit
+
+    start = timeit.default_timer()
     preprocess(sys.argv[1])
+    print('run time:', timeit.default_timer() - start)
 
