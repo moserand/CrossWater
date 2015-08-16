685548e6a8c42132daf2de52f6d0a689fd75e9a8
Mike Mueller <mmueller@hydrocomputing.com>
2015-08-16 12:51:20 +0200

    Add progess display.

--- crosswater/tools/time_helper.py
+++ crosswater/tools/time_helper.py
@@ -1,6 +1,8 @@
 """Time measuring and display helper
 """
 
+# pylint: disable=star-args
+
 from collections import namedtuple
 from functools import wraps
 from timeit import default_timer
@@ -19,10 +21,11 @@ def seconds_to_time_tuple(seconds):
     decimal, rest = divmod(rest, 0.1)
     if seconds < 1:
         decimal = max(decimal, 1)
-    Delta = namedtuple('timedelta', ['days', 'hours', 'minutes', 'seconds',
+    delta = namedtuple('timedelta', ['days', 'hours', 'minutes', 'seconds',
                                      'decimal'])
-    return Delta(*[int(entry) for entry in (days, hours, minutes, sec,
+    return delta(*[int(entry) for entry in (days, hours, minutes, sec,
                                             decimal)])
+
 
 def format_time_tuple(time_tuple, fixed=False):
     """Make the time nicely human readbale.
@@ -42,8 +45,10 @@ def format_time_tuple(time_tuple, fixed=False):
         return '{:2d} s'.format(seconds)
 
 
-def format_seconds(seconds):
-    return(format_time_tuple(seconds_to_time_tuple(seconds)))
+def format_seconds(seconds, fixed=False):
+    """Format seconds with `format_time_tuple`.
+    """
+    return format_time_tuple(seconds_to_time_tuple(seconds), fixed=fixed)
 
 
 def show_used_time(func):
@@ -51,10 +56,76 @@ def show_used_time(func):
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start = default_timer()
-        res = func(*args, **kwargs)
-        seconds = default_timer() - start
-        print()
-        print('run time:', format_seconds(seconds))
+        """Wrapper function.
+        """
+        try:
+            start = default_timer()
+            res = func(*args, **kwargs)
+        finally:
+            seconds = default_timer() - start
+            print()
+            print('run time:', format_seconds(seconds))
         return res
     return wrapper
+
+
+class ProgressDisplay(object):
+    """Show progess with time information on screen.
+    """
+    # pylint: disable=too-few-public-methods
+    def __init__(self, total, interval=0.3, additional_format='{:s}'):
+        self.start = default_timer()
+        self.total = total
+        self.interval = interval
+        self.additional_format = additional_format
+        self.last_display = self.start
+        self.total_width = len(str(total))
+
+    def show_progress(self, counter, force=False, additional=None):
+        """Display a progress bar with time imformation inplace.
+
+        No line break.
+        """
+        current_stamp = default_timer()
+        if force or current_stamp - self.last_display >= self.interval:
+            self._process(current_stamp, counter, additional)
+            self.last_display = current_stamp
+
+    def _process(self, current_stamp, counter, additional):
+        """Show the information.
+        """
+        assert counter > 0
+        total = self.total
+        duration = current_stamp - self.start
+        fraction = counter / total
+        try:
+            estimated = duration / fraction
+        except ZeroDivisionError:
+            return
+        remaning = estimated - duration
+        percent = fraction * 100
+        text = ''
+        if additional:
+            text += self.additional_format.format(additional)
+        text += ' {:{total_width}d}/{:{total_width}d}'.format(
+            counter, total, total_width=self.total_width)
+        text += ' {:5.2f} %'.format(percent)
+        text += ' elapsed: ' + format_seconds(duration, fixed=True)
+        text += ' estimated: ' + format_seconds(estimated, fixed=True)
+        text += ' remaning: ' + format_seconds(remaning, fixed=True)
+        print(text, end='\r')
+
+if __name__ == '__main__':
+
+    @show_used_time
+    def test():
+        """Little test
+        """
+        import time
+        count = 1000
+        prog = ProgressDisplay(count)
+        for x in range(1, count):
+            time.sleep(0.05)
+            prog.show_progress(x, 12)
+
+    test()
