28c1282513f4ec32ecd7249bfb08babe5d05c29f
Mike Mueller <mmueller@hydrocomputing.com>
2015-08-16 13:17:35 +0200

    Improve main function.

--- crosswater/catchment_model/run.py
+++ crosswater/catchment_model/run.py
@@ -4,6 +4,7 @@
 import sys
 
 from crosswater.catchment_model.model_runner import ModelRunner
+from crosswater.catchment_model.convert_hdf import run_convertion
 from crosswater.tools.time_helper import show_used_time
 
 
@@ -11,8 +12,12 @@ from crosswater.tools.time_helper import show_used_time
 def run():
     """Run all catchment models.
     """
-    runner = ModelRunner(sys.argv[1])
+    config = sys.argv[1]
+    print('runing with {} ...'.format(config))
+    runner = ModelRunner(config)
     runner.run_all()
+
+    run_convertion(config, batch_size=100)
 
 
 if __name__ == '__main__':
