b5d22be5c0f39834c9b12c2c0872893ff4214fc1
Mike Mueller <mmueller@hydrocomputing.com>
2015-08-16 09:33:50 +0200

    Add formatting of time display. Add decimal to time tuple.

--- crosswater/tools/time_helper.py
+++ crosswater/tools/time_helper.py
@@ -7,12 +7,40 @@ from timeit import default_timer
 
 
 def seconds_to_time_tuple(seconds):
+    """Convert seconds into a namedtuple.
+
+    Contains days, hours, minutes, seconds, andf one deciaml of the second.
+    """
+
     days, rest = divmod(seconds, 86400)
     hours, rest = divmod(rest, 3600)
-    minutes, sec = divmod(rest, 60)
-    Delta = namedtuple('timedelta', ['days', 'hours', 'minutes', 'seconds'])
-    return Delta(days, hours, minutes, sec)
+    minutes, rest = divmod(rest, 60)
+    sec, rest = divmod(rest, 1)
+    decimal, rest = divmod(rest, 0.1)
+    if seconds < 1:
+        decimal = max(decimal, 1)
+    Delta = namedtuple('timedelta', ['days', 'hours', 'minutes', 'seconds',
+                                     'decimal'])
+    return Delta(*[int(entry) for entry in (days, hours, minutes, sec,
+                                            decimal)])
 
+
+def format_time_tuple(time_tuple, fixed=False):
+    """Make the time nicely human readbale.
+    """
+    days, hours, minutes, seconds, decimal = time_tuple
+    if days:
+        return '{:2d} days {:02d}:{:02d}:{:02d}'.format(*time_tuple)
+    if fixed:
+        return '{:02d}:{:02d}:{:02d}'.format(*time_tuple[1:])
+    elif hours:
+        return '{:2d}:{:02d}:{:02d} hours'.format(*time_tuple[1:])
+    elif minutes:
+        return '{:2d}:{:02d} min'.format(*time_tuple[2:])
+    else:
+        if seconds < 10:
+            return '{:2d}.{:d} s'.format(seconds, decimal)
+        return '{:2d} s'.format(seconds)
 
 def show_used_time(func):
     """Measure run time and print after completion.
--- tests/test_time_helpers.py
+++ tests/test_time_helpers.py
@@ -1,7 +1,8 @@
 
 import time
 
-from crosswater.tools.time_helper import show_used_time, seconds_to_time_tuple
+from crosswater.tools.time_helper import (
+    show_used_time, seconds_to_time_tuple, format_time_tuple)
 
 
 def test_show_used_time_1(capfd):
@@ -29,11 +30,25 @@ def test_show_used_time_2(capfd):
 
 
 def test_seconds_to_tuple():
-    assert seconds_to_time_tuple(1) == (0, 0, 0, 1)
+    assert seconds_to_time_tuple(0) == (0, 0, 0, 0, 1)
+    assert seconds_to_time_tuple(0.5) == (0, 0, 0, 0, 4)
+    assert seconds_to_time_tuple(1) == (0, 0, 0, 1, 0)
     assert seconds_to_time_tuple(1).seconds == 1
-    assert seconds_to_time_tuple(61) == (0, 0, 1, 1)
-    assert seconds_to_time_tuple(661) == (0, 0, 11, 1)
-    assert seconds_to_time_tuple(7861) == (0, 2, 11, 1)
-    assert seconds_to_time_tuple(86400) == (1, 0, 0, 0)
-    assert seconds_to_time_tuple(86401) == (1, 0, 0, 1)
+    assert seconds_to_time_tuple(61) == (0, 0, 1, 1, 0)
+    assert seconds_to_time_tuple(661) == (0, 0, 11, 1, 0)
+    assert seconds_to_time_tuple(7861) == (0, 2, 11, 1, 0)
+    assert seconds_to_time_tuple(86400) == (1, 0, 0, 0, 0)
+    assert seconds_to_time_tuple(86401) == (1, 0, 0, 1, 0)
 
+
+def test_format_time_tuple():
+    assert format_time_tuple((0, 0, 0, 0, 1)) == ' 0.1 s'
+    assert format_time_tuple((0, 0, 0, 5, 0)) == ' 5.0 s'
+    assert format_time_tuple((0, 0, 0, 10, 0)) == '10 s'
+    assert format_time_tuple((0, 0, 1, 0, 0)) == ' 1:00 min'
+    assert format_time_tuple((0, 2, 1, 4, 0)) == ' 2:01:04 hours'
+    assert format_time_tuple((5, 2, 1, 4, 0)) == ' 5 days 02:01:04'
+    assert format_time_tuple((0, 0, 0, 10, 0), fixed=True) == '00:00:10'
+    assert format_time_tuple((0, 0, 1, 0, 0), fixed=True) == '00:01:00'
+    assert format_time_tuple((0, 2, 1, 4, 0), fixed=True) == '02:01:04'
+    assert format_time_tuple((5, 2, 1, 4, 0), fixed=True) == ' 5 days 02:01:04'
