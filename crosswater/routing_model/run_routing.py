089ee4f49d910eac92950554d14f6bc080c34c07
Andreas Moser <andreas.moser@eawag.ch>
2016-11-07 13:50:44 +0100

    Working version for biocides/herbicides

--- crosswater/routing_model/run_routing.py
+++ crosswater/routing_model/run_routing.py
@@ -17,20 +17,20 @@ def run_routing():
     """
     config_file = sys.argv[1]
     
-#    print('Divide river network into compartments and aggregate loads')
-#    tributaries = Tributaries(config_file)    
-#    compartments = Compartments(config_file, tributaries)
-#    links = Links(config_file, tributaries, compartments)
-#    parameterization = Parameterization(config_file, compartments)
-#    initialconditions = InitialConditions(config_file, compartments, links)
-#    aggregate = Aggregate(config_file, tributaries, compartments, links, parameterization, initialconditions)       
-#    aggregate.run()
-#    print('')
-#    
-#    print('Run conversion from one table per timestep to one table per compartment')
-#    conversion = Convert(config_file)
-#    conversion.run()
-#    print('')
+    print('Divide river network into compartments and aggregate loads')
+    tributaries = Tributaries(config_file)    
+    compartments = Compartments(config_file, tributaries)
+    links = Links(config_file, tributaries, compartments)
+    parameterization = Parameterization(config_file, compartments)
+    initialconditions = InitialConditions(config_file, compartments, links)
+    aggregate = Aggregate(config_file, tributaries, compartments, links, parameterization, initialconditions)       
+    aggregate.run()
+    print('')
+    
+    print('Run conversion from one table per timestep to one table per compartment')
+    conversion = Convert(config_file)
+    conversion.run()
+    print('')
     
     print('Write Aquasim input file')
     write_aqu(config_file)
