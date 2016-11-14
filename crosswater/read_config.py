4e7dfbb3372fecba36aad9db6aeaaf3e14b1fb99
Andreas Moser <andreas.moser@eawag.ch>
2016-11-14 13:44:17 +0100

    Adapt C_back by ratio of herbicide application area and total catchment area

--- crosswater/read_config.py
+++ crosswater/read_config.py
@@ -30,6 +30,7 @@ def read_config(file_name_or_fobj):
     batch_size = config['preprocessing'].getint('batch_size', fallback=None)
     strahler_limit = config['preprocessing'].getint('strahler_limit',
                                                     fallback=None)
+    ratio_herbicide_area_cal = config['catchment_model'].getfloat('ratio_herbicide_area_cal', fallback=None)
     res = {sec_name: make_abs_paths(config, sec_name) for
            sec_name in config.sections()}
     res['catchment_model']['number_of_workers'] = int(
@@ -37,4 +38,5 @@ def read_config(file_name_or_fobj):
     res['preprocessing']['max_ids'] = max_ids
     res['preprocessing']['batch_size'] = batch_size
     res['preprocessing']['strahler_limit'] = strahler_limit
+    res['catchment_model']['ratio_herbicide_area_cal'] = ratio_herbicide_area_cal
     return res
